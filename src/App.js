cbb182fb6981524bb3f980d6d3921e0d6711a1ed
Tran Nhu Long <nhulongctk10@gmail.com>
2017-12-14 13:05:32 +0700

    Add react button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Button } from './components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container-fluid">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          Button color
+          <p>
+            <Button color="secondary">LongTran</Button>
+            <Button color="primary">LongTran</Button>
+            <Button color="success">LongTran</Button>
+            <Button color="danger">LongTran</Button>
+            <Button color="warning">LongTran</Button>
+            <Button color="link">LongTran</Button>
+          </p>
+         </div>
+         <div>
+          Button tag
+          <p>
+            <Button href="#" role="button">LongTran</Button>
+            <Button>LongTran</Button>
+          </p>
+         </div>
+         <div>
+          Button outline
+          <p>
+            <Button outline color="secondary">LongTran</Button>
+            <Button outline color="primary">LongTran</Button>
+            <Button outline color="success">LongTran</Button>
+            <Button outline color="danger">LongTran</Button>
+            <Button outline color="warning">LongTran</Button>
+            <Button outline color="light">LongTran</Button>
+            <Button outline color="dark">LongTran</Button>
+          </p>
+         </div>
+         <div>
+          Button size
+          <p>
+            <Button size="sm" color="secondary">LongTran</Button>
+            <Button color="primary">LongTran</Button>
+            <Button size="lg" color="success">LongTran</Button>
+          </p>
+         </div>
+         <div>
+          Button block
+          <p>
+            <Button block color="secondary">LongTran</Button>
+            <Button block color="primary">LongTran</Button>
+          </p>
+         </div>
+         <div>
+          Button disable state
+          <p>
+            <Button disabled color="secondary">LongTran</Button>
+            <Button disabled color="primary">LongTran</Button>
+          </p>
+         </div>
       </div>
     );
   }
---
+++ src/components/Button.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const ButtonProps = {
+  /**
+   * Value: button | a | input
+   * @param {tag}
+   */
+  tag: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.string,
+  ]),
+
+  /**
+   * Button will active when clicked.
+   * However, you can still force with .active
+   * @param {active}
+   */
+  active: PropTypes.bool,
+
+  /**
+   * Value: primary | secondary | success | info | warning | danger
+   * @param {color}
+   *
+   */
+  color: PropTypes.string,
+  disabled: PropTypes.bool,
+
+  /**
+   * Value: lg | sm
+   * @param {size}
+   */
+  size: PropTypes.string,
+  class: PropTypes.string,
+  block: PropTypes.bool,
+  outline: PropTypes.bool,
+
+  /**
+   * Value: button | submit | reset | undefined
+   * @param {type}
+   */
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+const Button = (props) => {
+  const handleOnClick = (event) => {
+    if (props.disabled) {
+      event.preventDefault();
+      return;
+    }
+
+    props.onClick(event);
+  }
+
+  let {
+    tag: Tag,
+    active,
+    block,
+    disabled,
+    outline,
+    color,
+    size,
+    type,
+    ...attributes,
+  } = props;
+
+  const btnClasses = classNames(
+    "btn",
+    `btn${outline ? '-outline' : ''}-${color}`,
+    block ? "btn-block" : false,
+    size ? `btn-${size}` : false,
+    { active }
+  );
+
+  if (attributes.href) {
+    Tag = 'a';
+  }
+
+  const btnType = Tag === 'button' && attributes.onClick
+    ? type
+    : undefined;
+
+  return (
+    <Tag
+      type={btnType}
+      className={btnClasses}
+      onClick={handleOnClick}
+      disabled={disabled}
+      {...attributes}
+    />
+  )
+}
+
+Button.propTypes = ButtonProps;
+Button.defaultProps = {
+  tag: 'button',
+  color: 'secondary',
+  type: 'button',
+  onClick: () => {},
+}
+
+export default Button;
